adaec2e21890c3d528e89bb8b2a2d73ba872a819
sarath.kn <sarath.kn@robosoftin.com>
2017-03-27 19:21:32 +0530

    initial commit

---
+++ app/src/main/java/com/sarath/dateslidersample/widgets/DateSlider/labeler/MinuteLabeler.java
@@ -0,0 +1,31 @@
+package com.sarath.dateslidersample.widgets.DateSlider.labeler;
+
+import com.sarath.dateslidersample.widgets.DateSlider.TimeObject;
+
+import java.util.Calendar;
+/**
+ * A Labeler that displays minutes
+ */
+public class MinuteLabeler extends Labeler {
+    private final String mFormatString;
+
+    public MinuteLabeler(String formatString) {
+        super(45, 60);
+        mFormatString = formatString;
+    }
+
+    @Override
+    public TimeObject add(long time, int val) {
+        return timeObjectfromCalendar(Util.addMinutes(time, val, minuteInterval));
+    }
+
+    @Override
+    protected TimeObject timeObjectfromCalendar(Calendar c) {
+    	if (minuteInterval>1) {
+    		int minutes = c.get(Calendar.MINUTE);
+    		c.set(Calendar.MINUTE, minutes-(minutes%minuteInterval));
+    	}
+        return Util.getMinute(c, mFormatString, minuteInterval);
+    }
+
+}
